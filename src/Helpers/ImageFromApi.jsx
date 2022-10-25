6a7eb795a8d91bc8a354a0d4534c8286797d5e50
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-10-25 20:04:24 +0300

    fix bug

--- src/Helpers/ImageFromApi.jsx
+++ src/Helpers/ImageFromApi.jsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-const imageApi = axios.create({ baseURL: 'https://pixabay.com/api' });
-// axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const fetchImages = async (imageName, pageNumber) => {
-  const { data } = await imageApi.get(
+  const { data } = await axios.get(
     `/?q=${imageName}&page=${pageNumber}&key=30007287-c01b0e4a3e2dffba00e51bbf9&image_type=photo&orientation=horizontal&per_page=12`
   );
   return data.hits;
 };
 
 export default fetchImages;
+
+// const imageApi = axios.create({ baseURL: 'https://pixabay.com/api' });
 // const imageApi = axios.create({
 //   baseURL: 'https://pixabay.com/api',
 //   // params: {
